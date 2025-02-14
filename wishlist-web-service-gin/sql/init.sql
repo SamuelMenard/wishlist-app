0b86501650cbdf13d4752f0d113fa5bdd3680c96
Samuel Menard <samuel.menard@videotron.ca>
2025-02-14 08:54:13 -0500

    SQL init file

---
+++ wishlist-web-service-gin/sql/init.sql
@@ -0,0 +1,46 @@
+DROP TABLE users;
+DROP TABLE wishlists;
+DROP TABLE users_wishlists;
+DROP TYPE wishlist_role;
+
+-- Enable extensions
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- Create enums
+CREATE TYPE wishlist_role AS ENUM ('owner', 'collaborator');
+
+-- Create tables
+CREATE TABLE users (
+    user_id BIGINT UNIQUE NULLS NOT DISTINCT GENERATED ALWAYS AS IDENTITY,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT CONSTRAINT email_unique UNIQUE NOT NULL,
+	CONSTRAINT pk_users PRIMARY KEY (user_id)
+);
+
+CREATE TABLE wishlists (
+    wishlist_id BIGINT UNIQUE NULLS NOT DISTINCT GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+	CONSTRAINT pk_wishlists PRIMARY KEY (wishlist_id)
+);
+
+CREATE TABLE users_wishlists (
+    id BIGINT UNIQUE NULLS NOT DISTINCT GENERATED ALWAYS AS IDENTITY,
+    user_id  BIGINT REFERENCES users,
+    wishlist_id BIGINT REFERENCES wishlists,
+    user_role wishlist_role NOT NULL,
+    CONSTRAINT pk_users_wishlists PRIMARY KEY (user_id, wishlist_id, id)
+);
+
+-- Create triggers
+CREATE TRIGGER before_insert_users
+BEFORE INSERT ON users
+FOR EACH ROW
+EXECUTE FUNCTION set_created_at();
+
+-- Insert dummy data
+INSERT INTO users (username, password, email) VALUES ('admin', crypt('root', gen_salt('bf')), 'admin@test.com');
+INSERT INTO wishlists (name) VALUES ('My very first wishlist');
+INSERT INTO users_wishlists (user_id, wishlist_id, user_role) VALUES (1, 1, 'owner');
